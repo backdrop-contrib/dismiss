d6dd2d326f7808d96e109a69a247e79ad1443fad
PackWeb <info@packweb.eu>
2020-11-03 23:24:50 +1100

    Port to Backdrop.

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
 Dismiss
 =======
 
-This module adds jQuery-powered Dismiss buttons to each block of messages that
-Drupal outputs. There are three default types: status, warning, and error
+This module allows Backdrop messages to be dismissed via buttons that are added
+to each block of messages. Additionally, a timeout can be set so status messages
+can be automatically dismissed (warnings and errors are never automatically
+dismissed). These features allow users to quickly close messages that might
+otherwise get in the way or cause distractions.
 
-The Dismiss button allows you to quickly close messages and errors that get in
-the way of what you're doing. For themers, it's an easy way to sidestep other
-PHP errors that might be in your codebase. For end-users and site admins, it's
-a simple way to make your website a tad more user-friendly.
+Installation
+------------
+
+- Install this module using the official Backdrop CMS instructions at
+  https://backdropcms.org/guide/modules.
+
+- Visit the configuration page under Administration > Configuration > User
+  Interface > Dismiss (admin/config/user-interface/dismiss) and configure the
+  settings as desired.
+
+Issues
+------
+
+Bugs and Feature requests should be reported in the Issue Queue:
+https://github.com/backdrop-contrib/dismiss/issues.
+
+Current Maintainers
+-------------------
+
+- [Peter Anderson](https://github.com/BWPanda)
+
+Credits
+-------
+
+- Ported to Backdrop CMS by [Peter Anderson](https://github.com/BWPanda).
+- Originally written for Drupal by [Chris Ruppel](https://github.com/rupl).
+
+License
+-------
+
+This project is GPL v2 software.
+See the LICENSE.txt file in this directory for complete text.
--- js/dismiss.js
+++ js/dismiss.js
@@ -1,35 +1,35 @@
 /**
  * @file
- *   Main JavaScript file for Dismiss module
+ * Main JavaScript file for Dismiss module.
  */
 
-(function ($) {
+(function($) {
 
-  Drupal.behaviors.dismiss = {
-    attach: function (context, settings) {
+Backdrop.behaviors.dismiss = {
+  attach: function(context, settings) {
 
-      // Prepend the Dismiss button to each message box.
-      $('.messages').each(function () {
-        var flag = $(this).children().hasClass('dismiss');
-
-        if (!flag) {
-          $(this).prepend('<button class="dismiss"><span class="element-invisible">' + Drupal.t('Close this message.') + '</span></button>');
-        }
-      });
-
-      // When the Dismiss button is clicked hide this set of messages.
-      $('.dismiss').click(function (event) {
-        $(this).parent().hide('fast');
-        // In case this message is inside a form, prevent form submission.
-        event.preventDefault();
-      });
-
-      // Fadeout out status messages when positive value defined.
-      if (Drupal.settings.dismiss.fadeout > 0) {
-        setTimeout(function () { $('.messages.status').fadeOut(); }, Drupal.settings.dismiss.fadeout);
+    // Prepend the Dismiss button to each message box.
+    $('.messages').each(function() {
+      if ($(this).children().not('.dismiss')) {
+        $(this).prepend('<button class="dismiss"><span class="element-invisible">' + Backdrop.t('Close this message.') + '</span></button>');
       }
+    });
 
+    // When the Dismiss button is clicked, hide this set of messages.
+    $('.dismiss').click(function(event) {
+      $(this).parent().hide('fast');
+      // In case this message is inside a form, prevent form submission.
+      event.preventDefault();
+    });
+
+    // Fadeout status messages when positive value defined.
+    if (Backdrop.settings.dismiss.fadeout > 0) {
+      setTimeout(function() {
+        $('.messages.status').fadeOut();
+      }, Backdrop.settings.dismiss.fadeout);
     }
+
   }
+};
 
 })(jQuery);
